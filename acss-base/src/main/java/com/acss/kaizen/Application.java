00e7f72569b31ab39ce137f80ecb5e942a216828
ultragaylord <vargas.gail@gmail.com>
2014-05-24 15:33:46 +0800

    Initial commit

---
+++ acss-base/src/main/java/com/acss/kaizen/Application.java
@@ -0,0 +1,3 @@
+package com.acss.kaizen;
+
+public interface Application {}
---
+++ acss-base/src/main/java/com/acss/kaizen/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.acss.kaizen.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+
+import com.acss.kaizen.account.UserService;
+
+@Configuration
+@ImportResource(value = "classpath:spring-security-context.xml")
+class SecurityConfig {
+	
+	@Bean
+	public UserService userService() {
+		return new UserService();
+	}
+
+	@Bean
+	public TokenBasedRememberMeServices rememberMeServices() {
+		return new TokenBasedRememberMeServices("remember-me-key", userService());
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new StandardPasswordEncoder();
+	}
+}
